393dc3c607ec22ece6c321a0366ac9d298a94c33
SteffRhes <stefanresch@protonmail.com>
2024-04-19 11:26:39 +0200

    turn specifics into templates with regard to model architecture and python environment

--- src/specifics_eval_model.py
+++ src/specifics_eval_model.py
@@ -1,25 +1,19 @@
 import os
 
-import fasttext
-import numpy as np
-import yaml
-
 
 # eval metadata
 EVAL_DATA_PATH = "/veld/input/2/eval_data.yaml"
 EVAL_SUMMARY_PATH = "/veld/output/summary.yaml"
 
-# model metadata
-MODEL_PATH = "/veld/input/1/model.bin"
+# model metadata 
+# TODO: ADAPT THIS TO YOUR SETUP
+MODEL_PATH = None
 MODEL_ARCH = os.environ.get("MODEL_ARCH")
 MODEL_ID = os.environ.get("MODEL_ID")
 MODEL_INFO = None
-with open("/veld/input/1/metadata.yaml", "r") as f:
-    MODEL_INFO = yaml.safe_load(f)
-    MODEL_INFO["training_reproducible_at"]: os.environ.get("MODEL_TRAIN_REPRODUCIBLE") 
 
 
-# override in respective branch
+# TODO: ADAPT THIS TO YOUR SETUP
 class ModelLogicContainer:
     """
     template class for all code dealing with model specifics
@@ -27,9 +21,9 @@ class ModelLogicContainer:
 
     def __init__(self):
         """
-        template method for any initialization logic
+        template method for any initialization logic. This method should not need any parameters.
         """
-        self.model = fasttext.load_model(MODEL_PATH)
+        pass
 
     def cos_sim_of_words(self, w1, w2):
         """
@@ -42,11 +36,5 @@ class ModelLogicContainer:
         Returns:
         float: cosine similarity, ranging from 0 to 1 
         """
-        v1 = self.model.get_word_vector(w1)
-        v2 = self.model.get_word_vector(w2)
-        dp = np.dot(v1, v2)
-        nv1 = np.linalg.norm(v1)
-        nv2 = np.linalg.norm(v2)
-        s = dp / (nv1 * nv2)
-        return s
+        pass
 
