7355403e6f8131249fbb6c536459ed459537d63a
SteffRhes <stefanresch@protonmail.com>
2024-05-06 11:53:10 +0200

    update evaluation logic, improve veld.yaml self documenatation

--- src/specifics_eval_model.py
+++ src/specifics_eval_model.py
@@ -3,31 +3,24 @@ import os
 import yaml
 
 
-# file mounts, model
-MODEL_PATH = "/veld/input/model.bin"
-MODEL_INFO_PATH = "/veld/input/metadata.yaml"
+# model data
+MODEL_PATH = os.getenv("model_path")
+if MODEL_PATH is None:
+    raise Exception("no model_path defined.")
+MODEL_METADATA_PATH = os.getenv("model_metadata_path")
+if MODEL_METADATA_PATH is None:
+    raise Exception("no model_metadata_path defined.")
 
-# file mounts, evaluation
-EVAL_DATA_PATH = "/veld/input/eval_data.yaml"
-EVAL_SUMMARY_PATH = "/veld/output/summary.yaml"
-EVAL_LOG_PATH = "/veld/output/logs/"
 
-# environment metadata
-MODEL_ARCH = os.environ.get("MODEL_ARCH")
-MODEL_ID = os.environ.get("MODEL_ID")
-MODEL_TRAIN_REPRODUCIBLE = os.environ.get("MODEL_TRAIN_REPRODUCIBLE")
-
-# load optional meta info, if it exists
-MODEL_INFO = None
+# load optional meta info
+MODEL_METADATA = {}
 try:
-    with open(MODEL_INFO_PATH, "r") as f:
-        MODEL_INFO = yaml.safe_load(f)
-        MODEL_INFO["training_reproducible_at"]: MODEL_TRAIN_REPRODUCIBLE
+    with open(MODEL_METADATA_PATH, "r") as f:
+        MODEL_METADATA = yaml.safe_load(f)
 except:
     pass
 
 
-# TODO: ADAPT THIS TO YOUR SETUP
 class ModelLogicContainer:
     """
     template class for all code dealing with model specifics
@@ -37,6 +30,9 @@ class ModelLogicContainer:
         """
         template method for any initialization logic. This method should not need any parameters.
         """
+	self.metadata = MODEL_METADATA
+
+	# TODO: implement initialization of model here
         pass
 
     def cos_sim_of_words(self, w1, w2):
@@ -50,5 +46,7 @@ class ModelLogicContainer:
         Returns:
         float: cosine similarity, ranging from 0 to 1 
         """
+
+	# TODO: implement vector comparision here
         pass
 
